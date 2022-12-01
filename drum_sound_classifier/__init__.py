075251853ddca098b63e91ad2a8f7393df48f19d
Keith Mitchell <keith.mitchell@live.ca>
2022-12-01 01:51:30 -0800

    feat: add additional map for drum aliases

--- drum_sound_classifier/__init__.py
+++ drum_sound_classifier/__init__.py
@@ -1,1 +1,9 @@
 DRUM_TYPES = ['hat', 'tom', 'ride', 'open', 'kick', 'bongo', 'clap', 'snare', 'rim', 'snap', 'crash', 'shaker']
+DRUM_TYPE_ALIASES = {
+  'ride': ['rid'],
+  'hat': ['hh'],
+  'kick': ['bd'],
+  'clap': ['clp'],
+  'snare': ['sd', 'sn'],
+  'crash': ['crs', 'cym']
+}
